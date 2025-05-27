7f4bb220cec006dcf91526914c505e6a70efc439
Herve Desrosiers <hdesrosiers@stingray.com>
2025-05-27 11:32:26 -0400

    refactor(swimlane): remove unnecessary wrapper and class from Swimlane component
    - Swimlane now renders children directly using a React fragment
    - Eliminates nested .content-swimlane structure in the DOM
    - Simplifies swimlane layout and improves maintainability

--- src/components/Swimlane.jsx
+++ src/components/Swimlane.jsx
@@ -1,18 +1,6 @@
 import React from 'react';
-import '../styles/App.css';
 
-const Swimlane = React.forwardRef(({ children, ...props }, ref) => {
-  return (
-    <div 
-      className="content-swimlane"
-      ref={ref}
-      {...props}
-    >
-      {children}
-    </div>
-  );
-});
-
-Swimlane.displayName = 'Swimlane';
-
-export default Swimlane; 
+export default function Swimlane({ children }) {
+  // Render children directly with no extra wrapper or class
+  return <>{children}</>;
+} 
